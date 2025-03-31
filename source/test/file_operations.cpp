3db6ff159a11407e98f48a787f63c608f7158c78
Axilos <zenos_22@hotmail.com>
2025-03-31 16:48:36 +0200

    Add tests
    * Test for file operations
    * Test for OpenGL (not passing)

---
+++ source/test/file_operations.cpp
@@ -0,0 +1,74 @@
+#include "file_operations.hpp"
+
+enum Scenario {
+    EMPTY_RUN,
+    FILE_CHECK
+};
+
+using namespace std;
+
+constexpr int MIN_ARGUMENT_NUMBER = 2;
+constexpr int NORMAL_ARGUMENT_NUMBER = 2;
+char const* SHADERS_PATH = "../shaders";
+
+void print_all_args(int argc, char** argv) {
+    for(int argn=0; argn<argc; argn++) {
+        cout << "[" << argn << "] = " << argv[argn] << endl;
+    }
+}
+
+int empty_run() {
+    string test = "abcd";
+    return 0;
+}
+
+int file_check() {
+    ifstream file;
+    string line;
+
+    string shaders1(SHADERS_PATH);
+    shaders1 += "/fragment.txt";
+    
+    file.open(shaders1);
+    if(!file.is_open()) {
+        cout << "Error while opening the file:" << shaders1 << endl;
+        return -1;
+    }
+    auto line_counter = 0;
+    while(getline(file, line)) {
+        cout << "file[" << line_counter << "] = " << line;
+    }
+    file.close();
+    return 0;
+}
+
+int main(int argc, char** argv)  {
+    if (argc < MIN_ARGUMENT_NUMBER) {
+        cout << "Missing Scenario argument" << endl;
+        return -1;
+    }
+    if(argc > NORMAL_ARGUMENT_NUMBER) {
+        cout << "Many argument provided, printing them:" << endl;
+        print_all_args(argc, argv);
+        return 0;
+    }
+    auto scenar = static_cast<Scenario>(stoi(argv[1]));
+    switch (scenar)
+    {
+    case EMPTY_RUN:
+        cout << "empty_run" << endl;
+        return empty_run();
+        break;
+
+    case FILE_CHECK:
+        cout << "file_check" << endl;
+        return file_check();
+        break;
+
+    default:
+        cout << "Unplanned Scenario, printing all args" << endl;
+        print_all_args(argc, argv);
+        break;
+    }
+    return 0;
+}
---
+++ source/test/opengl_tester.cpp
@@ -0,0 +1,81 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "file_operations.hpp"
+
+enum Scenario {
+    GLFW_INIT,
+    GLAD_INIT,
+    OPENGL_WINDOW
+};
+
+using namespace std;
+
+constexpr int MIN_ARGUMENT_NUMBER = 2;
+constexpr int NORMAL_ARGUMENT_NUMBER = 2;
+
+int glfw_init() {
+    int res = GLFW_FALSE;
+    try {
+        res = glfwInit();
+    } catch (exception& e) {
+        cout << e.what() << endl;
+    }
+    return res;
+}
+
+int glad_init() {
+    if (gladLoadGL() == 0) {
+        return -1;
+    }
+    return 0;
+}
+
+int openlg_window(const int width=640, const int height=480, const int timeout = 2000) {
+    auto gl_ok = glfw_init();
+    auto glad_ok = glad_init();
+    if(gl_ok && glad_ok) {
+        auto window = glfwCreateWindow(width, height, "Test Window", nullptr, nullptr);
+        glfwMakeContextCurrent(window);
+        auto time = 0;
+        while(time < timeout) {
+            glfwPollEvents();
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            time++;
+        }
+    }
+}
+
+int main(int argc, char** argv)  {
+    if (argc < MIN_ARGUMENT_NUMBER) {
+        cout << "Missing Scenario argument" << endl;
+        return -1;
+    }
+    auto scenar = static_cast<Scenario>(stoi(argv[1]));
+    // auto width = stoi(argv[1]);
+    // auto height = stoi(argv[2]);
+    switch (scenar)
+    {
+    case GLFW_INIT:
+        cout << "glfw_init" << endl;
+        return glfw_init();
+        break;
+
+    case GLAD_INIT:
+        cout << "glad_init" << endl;
+        return glad_init();
+        break;
+
+    case OPENGL_WINDOW:
+        cout << "openlg_window" << endl;
+        return openlg_window();
+        break;
+
+    default:
+        cout << "Unplanned Scenario, exit" << endl;
+        return -1;
+        break;
+    }
+    return 0;
+}
